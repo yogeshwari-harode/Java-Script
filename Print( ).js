ab75487a61d33f94127b6b6e7c9cfd1f3d3f82e9
Yogeshwari Harode <67998331+yogeshwari-harode@users.noreply.github.com>
2021-07-28 00:06:30 +0530

    Create Print( ).js

---
+++ Print( ).js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>javascript</title>
+	<style>
+		
+		body{
+			background-color: tan;
+			text-align: center;
+		}
+		button{
+			margin: 150px;
+			background-color: #e74c3c;
+			border: none;
+			padding: 12px 50px;
+			border-radius: 20px;
+			box-shadow: 2px 4px 5px;
+		}
+
+	</style>
+</head>
+<body>
+
+<button type="button" onclick="print()">Click me</button>
+
+</body>
+</html>
